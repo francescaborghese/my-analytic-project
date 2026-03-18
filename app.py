5156f23e0f6cf32102230418fb1633b8dbfd0106
francescaborghese <francescaborghese4@gmail.com>
2026-03-18 15:59:20 +0000

    Update README

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-print ("hello from my codespace enviroment")
+print("Hello from my codespace enviroment")
+
+import streamlit as st
+import pandas as pd
+
+st.title("Analazying Car Sharing Data with Streamlit ")
+st.sidebar.header("This is sidebar section")
